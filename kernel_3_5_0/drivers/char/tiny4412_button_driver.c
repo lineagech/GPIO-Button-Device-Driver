3e3f383f5b7af1ef325bbce7609c0d538604c1b0
Chia-Hao Chang <lineagech@gmail.com>
2018-10-09 17:29:30 +0800

    update code for init function and getting gpio

--- kernel_3_5_0/drivers/char/tiny4412_button_driver.c
+++ kernel_3_5_0/drivers/char/tiny4412_button_driver.c
@@ -9,8 +9,9 @@
 #include <linux/irq.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
-
 #include <linux/timer.h>
+
+#include <mach/gpio.h>
 
 #define DEVNUM_COUNT 			1
 #define DEVNUM_NAME 			"buttons"
@@ -46,8 +47,20 @@ static DECLARE_WAIT_QUEUE_HEAD(button_waitq);
 static unsigned int button_press = 0;
 
 void gpio_button_timer_hanlder(unsigned long _data)
-{
+{	
+	struct button_dev* dev = (struct button_dev*)_data;
+	// how should I get gpio input register value?
+	//int read_value = *(dev->gpio); //?
+	int read_value = gpio_get_value(dev->gpio);
 	
+	printk(KERN_DEBUG "BUTTON %d: %08x\n", dev->number, read_value);
+	if(read_value == 0) button_press = 1;
+	
+	if( button_press )
+	{	
+		button_press = 0;
+		wake_up_interruptible(button_waitq);	
+	}
 }
 
 static irqreturn_t gpio_interrupt_handler (int irq, void *dev_id)
@@ -72,12 +85,46 @@ static irqreturn_t gpio_interrupt_handler (int irq, void *dev_id)
   * */
  
 static int gpio_button_open (struct inode* inode, struct file* file)
-{
-	
+{	
+	for( i = 0; i < ARRAY_SIZE(buttons); i++ )
+	{
+		/* Set up timer */
+		init_timer( &button_dev[i].timer );
+		setup_timer( &button_dev[i].timer, gpio_button_timer_hanlder, &button_dev[i] );
+		
+		/* Initialize IRQ */
+		irq = gpio_to_irq(button_dev[i].gpio); // translate to irq number
+		err = request_irq(irq, gpio_interrupt_handler, IRQ_TYPE_EDGE_FALLING, 
+			  	  button_dev[i].name, (void *)&button_dev[i]);
+		
+		if( err ){
+			init_err_hanlder(i);
+			break;	
+		}
+	}
 }
 static ssize_t gpio_button_read (struct file* file, char __user* buff, size_t count, loff_t* p_off)
-{
+{	
+	// how can I get struct button_dev?
 	
+	int i;
+	int irq;
+	int err;
+	for( i = 0; i < ARRAY_SIZE(buttons); i++ )
+	{	
+		/* Unregister major/minor number */
+		int major_num = MAJOR(devno);
+		int minor_num = MINOR(devno);
+		unregister_chrdev_region(MKDEV(major_num, minor_num), DEVNUM_MINOR_START, DEVNUM_COUUNT , DEVNUM_NAME);
+		
+		/* Delete timer */
+		del_timer_sync(&button_dev[i].timer);
+		
+		/* Close irq */
+		irq = gpio_to_irq(button_dev[i].gpio);
+		disable_irq(irq);
+		free_irq(irq, (void *)&button_dev[i]);
+	}
 }
 static int gpio_button_close(struct inode* inode, struct file* file)
 {
@@ -121,59 +168,27 @@ static int __init tiny4412_buttons_init(void)
 	int i;
 	int irq;
 	int err;
-	for( i = 0; i < ARRAY_SIZE(buttons); i++ )
-	{
-		/* Allocate Device Major Number and Minor Number */
-		printk(KERN_INFO "dynamically allocate major and minor number...\n");
-		int alloc_re = 0;
-		alloc_re = alloc_chrdev_region(&devno, DEVNUM_MINOR_START, DEVNUM_COUUNT, DEVNUM_NAME);
-		if( alloc_re ) {
-			printk(KERN_WARNING "%s : could not allocate device number", __func__);
-		}	
-		else {
-			printk(KERN_WARNING "%s : register with major number %l and minor number %l", __func__, MAJOR(dev), MINOR(dev));
-		}
-	
-		/* Register Character Devices */
-		buttons_setup_dev(&button_dev, i);
-		
-		/* Set up timer */
-		init_timer( &button_dev[i].timer );
-		setup_timer( &button_dev[i].timer, gpio_button_timer_hanlder, &button_dev[i] );
-		
-		/* Initialize IRQ */
-		irq = gpio_to_irq(button_dev[i].gpio); // translate to irq number
-		err = request_irq(irq, gpio_interrupt_handler, IRQ_TYPE_EDGE_FALLING, 
-				  button_dev[i].name, (void *)&button_dev[i]);
-		
-		if( err ){
-			init_err_hanlder(i);
-			break;	
-		}
+	//for( i = 0; i < ARRAY_SIZE(buttons); i++ )
+	//{
+	/* Allocate Device Major Number and Minor Number */
+	printk(KERN_INFO "dynamically allocate major and minor number...\n");
+	int alloc_re = 0;
+	alloc_re = alloc_chrdev_region(&devno, DEVNUM_MINOR_START, DEVNUM_COUNT, DEVNUM_NAME);
+	if( alloc_re ) {
+		printk(KERN_WARNING "%s : could not allocate device number", __func__);
+	}	
+	else {
+		printk(KERN_WARNING "%s : register with major number %l and minor number %l", __func__, MAJOR(dev), MINOR(dev));
 	}
+	
+	/* Register Character Devices */
+	buttons_setup_dev(&button_dev, i);
+	//}
 	printk("tiny4412_buttons_init completed!");
 }
 
 static int __exit tiny4412_buttons_exit(void)
 {
-	int i;
-	int irq;
-	int err;
-	for( i = 0; i < ARRAY_SIZE(buttons); i++ )
-	{	
-		/* Unregister major/minor number */
-		int major_num = MAJOR(devno);
-		int minor_num = MINOR(devno);
-		unregister_chrdev_region(MKDEV(major_num, minor_num), DEVNUM_MINOR_START, DEVNUM_COUUNT , DEVNUM_NAME);
-		
-		/* Delete timer */
-		del_timer_sync(&button_dev[i].timer);
-		
-		/* Close irq */
-		irq = gpio_to_irq(button_dev[i].gpio);
-		disable_irq(irq);
-		free_irq(irq, (void *)&button_dev[i]);
-	}
 	printk("tiny4412_buttons_exit completed!");
 }
 
